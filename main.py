aa8b2073d2e87b55b572e5b5fb460155bac7f4a7
Artur Averin <payavernus@gmail.com>
2025-02-14 20:25:14 +0300

    добавлено расписание и запуск с поиском

--- main.py
+++ main.py
@@ -16,9 +16,19 @@ CHAT_ID = os.getenv('CHAT_ID')
 
 # Список пользователей в формате (user_id, имя)
 USERS = [
-    (123456789, "Gekkon"),  # Замените на реальные ID и имена
-    (987654321, "Aibezwer"),
-    # Добавьте остальных пользователей
+    (637476473, "Никита Калинин"),
+    (944818724, "Андрей Белозерцев"),
+    (2012379285, "Семён Варзин"),
+    (795615948, "Дмитрий Круглов"),
+    (1303275983, "Валерия Песьякова"),
+    (5623885884, "Виталий Баймурзаев"),
+    (775411734, "Егор Копосов"),
+    (1164588090, "Кирилл Поспелов"),  
+    (685317770, "Артём Лапин"),
+    (5606121328, "Артемий Исаков"),
+    (7026933741, "Илья Сулоев"),
+    (402356989, "Дмитрий Попов"),
+    (850750045, "Максим Малов"),
 ]
 
 # Инициализация бота и диспетчера
@@ -27,11 +37,15 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer('Бот запущен и будет отправлять ежедневное сообщение')
+    await message.answer('Ищу пидора дня...')
+    # Ждем 5 секунд
+    await asyncio.sleep(5)
+    # Отправляем первое сообщение
+    await send_daily_message()
 
 async def send_daily_message():
     selected_user_id, selected_user_name = random.choice(USERS)
-    message = f"Пидор дня [‌{selected_user_name}](tg://user?id={selected_user_id}) - отвечает за все!"
+    message = f"Пидор дня [‌{selected_user_name}](tg://user?id={selected_user_id})!"
     await bot.send_message(
         chat_id=CHAT_ID, 
         text=message,
@@ -39,12 +53,17 @@ async def send_daily_message():
     )
 
 async def scheduler():
-    aioschedule.every().day.at("12:00").do(send_daily_message)
+    aioschedule.every().day.at("09:00").do(send_daily_message)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
 
 async def main():
+    # Отправляем первое сообщение при запуске
+    await bot.send_message(chat_id=CHAT_ID, text="Меня ебали, я сосал")
+    await asyncio.sleep(5)
+    await send_daily_message()
+    
     # Запускаем планировщик в отдельной задаче
     asyncio.create_task(scheduler())
     # Запускаем бота
